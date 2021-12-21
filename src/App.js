a4afa7bad10fa92b989db9fd35268f9f867420f1
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-12-20 20:55:35 -0500

    appbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import { links } from "./Utils/data";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#c4d0f7",
+        },
+        secondary: {
+            main: "#646282",
+        },
+    },
+});
+
+export default function TemporaryDrawer() {
+    const [state, setState] = React.useState({
+        left: false,
+    });
+
+    const toggleDrawer = (anchor, isOpen) => (event) => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: isOpen });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            onClick={toggleDrawer("left", true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            News
+                        </Typography>
+
+                        <Button color="inherit" color="secondary">
+                            Iniciar sesión
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Drawer
+                anchor={"left"}
+                open={state["left"]}
+                onClose={toggleDrawer("left", false)}
+            >
+                <Box
+                    sx={{ width: 250 }}
+                    role="presentation"
+                    onClick={toggleDrawer("left", false)}
+                    onKeyDown={toggleDrawer("left", false)}
+                >
+                    <Divider />
+                    <List>
+                        {links.map(({ id, text, url, icon }) => (
+                            <ListItem button key={id} href={url}>
+                                <ListItemIcon>{icon}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+        </ThemeProvider>
+    );
 }
-
-export default App;
